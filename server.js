7516c9bbf6cf5d2ef597f63884fc2e5d59aa4259
Ruth Salvador <ruth.abby.sz@gmail.com>
2017-07-06 09:31:25 -0500

    make it better

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
+
 const express = require('express');
 const app = express();
 
-//aquí  le decimos qque use los archivos estáticos y que se muestren al llamar a la ruta '/'
+
 app.use('/', express.static('public'));
 
-//aquí le decimos al framework desde que puerto
+
 
 app.set('port', (process.env.PORT || 5000));
 
